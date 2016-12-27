d13fe68abfe00d3b06d0954148580bd59d953a3b
Henry Mora <hank8617@gmail.com>
2016-12-26 20:12:03 -0500

    First commit and El Tiempo spider

---
+++ spiders/NewsSpider.py
@@ -0,0 +1,27 @@
+import scrapy
+from news_scraper.items import NewsScraperItem
+from scrapy.selector import Selector
+
+
+class NewsSpider(scrapy.Spider):
+    name = "eltiemponews"
+    allowed_domains = ["eltiempo.com"]
+    start_urls = ["http://www.eltiempo.com/archivo/buscar?q=ingeniero&producto=eltiempo&orden=reciente&pagina=%s&a=2016" % page for page in xrange(1,101)]
+
+    def parse(self, response):
+        sel = Selector(response)
+        articles = sel.select("//article")
+        for article in articles:
+            title = article.select("h2/a/text()").extract()
+            description = article.select("p/a/text()").extract()
+            date = article.select("time/text()").extract()
+            link = article.select("h2/a/@href").extract()
+
+            item = NewsScraperItem()
+            item['title'] = title
+            item['description'] = description
+            item['date'] = date
+            item['link'] = link
+
+            yield item
+            #yield scrapy.Request(str(link), self.parse)
