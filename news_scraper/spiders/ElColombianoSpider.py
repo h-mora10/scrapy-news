dbdcf9c40696028d2ae44844ee2419a2363d50c6
Henry Mora <hank8617@gmail.com>
2017-01-11 22:11:25 -0500

    El Colombiano... tener en cuenta q el límite del rango debe ser el número de páginas más 1... si son 44 páginas el rango es (1,45)

--- news_scraper/spiders/ElColombianoSpider.py
+++ news_scraper/spiders/ElColombianoSpider.py
@@ -8,19 +8,27 @@ class ElColombianoSpider(scrapy.Spider):
     name = "elcolombianonews"
     allowed_domains = ["elcolombiano.com"]
 
-    start_urls = ['http://www.elcolombiano.com/busqueda/-/search/"ingeniero+de+sistemas"/false/true/20040101/20141231/date/true/true/0/0/meta/0/0/5/%d' % page for page in xrange(0,33)]
+    start_urls = ['http://www.elcolombiano.com/busqueda/-/search/"ingeniero de sistemas"/false/false/19170109/20170109/date/true/true/0/0/meta/0/0/5/%d' % page for page in xrange(1,45)]
 
     def parse(self, response):
         sel = Selector(response)
-        articles = sel.select("//div[@class='noticia-resultado']")
+        articles = sel.xpath("//div[@class='noticia-resultado']")
 
 
 #confoto
         for article in articles:
-            description = article.select("div[@class='right']/a/p/text()").extract()
-            title = article.select("div[@class='right']/a/h3[@class='titulo-noticia']/text()").extract()
-            date = article.select("div[@class='left']/div[@class='fecha']/span/text()").extract()
-            link = "http://www.elcolombiano.com" + article.select("div[@class='right']/a/@href").extract()[0]
+            try:
+                description = article.xpath("div[@class='right']/a/p/text()").extract()
+                title = article.xpath("div[@class='right']/a/h3[@class='titulo-noticia']/text()").extract()
+                date = article.xpath("div[@class='left']/div[@class='fecha']/span/text()").extract()
+                link = "http://www.elcolombiano.com" + article.xpath("div[@class='right']/a/@href").extract()[0]
+            except:
+                print("NO FOTO")
+                description = article.xpath("div[@class='rightNoFoto']/a/p/text()").extract()
+                title = article.xpath("div[@class='rightNoFoto']/a/h3[@class='titulo-noticia']/text()").extract()
+                date = article.xpath("div[@class='left']/div[@class='fechaNoFoto']/span/text()").extract()
+                link = "http://www.elcolombiano.com" + article.xpath("div[@class='rightNoFoto']/a/@href").extract()[0]
+
             item = NewsScraperItem()
             item['title'] = title
             item['description'] = description
